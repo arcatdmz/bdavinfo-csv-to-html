52c2cf14288bfbb13fcb06ec030ed171e95c874e
Jun Kato <i@junkato.jp>
2019-11-10 17:06:40 +0900

    Finish creating HTML template

--- index.ts
+++ index.ts
@@ -1,21 +1,101 @@
 import csv from "csv-parser";
 import fs from "fs";
 
+const input = "csv/bdav.csv",
+  output = "html/bdav.html";
+
 const results = [];
 
 const columns = 3;
 
+const date = (() => {
+  const d = new Date();
+  let month = "" + (d.getMonth() + 1),
+    day = "" + d.getDate(),
+    year = d.getFullYear();
+
+  if (month.length < 2) month = "0" + month;
+  if (day.length < 2) day = "0" + day;
+
+  return [year, month, day].join("/");
+})();
+
 const style = `
-  body { font-family: "Noto Sans JP"; }
-  .wrapper { display: flex; }
-  .column { width: ${Math.floor((100 * 1000) / columns) /
-    1000}%; flex-grow: 1; }
-  h1 { font-size: 5mm; margin: 0 0 2mm 0; padding: 0; border: 1px solid #000; border-width: 0 0 1px 0; }
-  p { font-size: 4mm; margin: 0; padding: 0; }
-  p small { font-size: 3mm; }
+  body {
+    font-family: "Noto Sans JP";
+    margin: 0;
+    padding: 4mm 2mm;
+  }
+  .wrapper {
+    display: flex;
+    flex-direction: column;
+  }
+  .wrapper > .header {
+    flex-grow: 0;
+  }
+  .wrapper > .header > h1 {
+    margin: 0 2mm 3mm 2mm;
+    padding: 1mm 2mm 1.2mm 2mm;
+    background: #eee;
+    border: 1px solid #000;
+    border-width: 1px 0;
+    font-size: 4mm;
+  }
+  .wrapper > .header > h1 > .dim {
+    color: #888;
+  }
+  .wrapper > .content {
+    flex-grow: 1;
+    display: flex;
+    flex-wrap: wrap;
+    flex-direction: row;
+    width: 100%;
+    height: 100%;
+  }
+  .title {
+    width: ${Math.floor((100 * 1000) / columns) / 1000}%;
+    flex-grow: 1;
+    margin: 3mm 0;
+  }
+  .title > * {
+    margin: 0 2mm;
+  }
+  .title > h1 {
+    display: flex;
+    align-items: stretch;
+    font-size: 3mm;
+    padding: 0;
+    border: 1px solid #000;
+    border-width: 1px 0;
+    min-height: 11mm;
+  }
+  .title > h1 > .index {
+    flex-grow: 0;
+    background: #aaa;
+    min-width: 4.5mm;
+    padding: 1mm 2mm;
+    color: #fff;
+    text-align: right;
+  }
+  .title > h1 > .content {
+    flex-grow: 1;
+    padding: 1mm;
+    background: #eee;
+  }
+  .title > p {
+    font-size: 2.4mm;
+    margin: 2mm !important;
+    padding: 0;
+  }
+  .title > p:last-child {
+    margin-bottom: 0 !important;
+  }
+  .title > p small {
+    font-size: 1.8mm;
+  }
 `;
 
-fs.createReadStream("csv/bdav1.csv", {
+fs.createReadStream(input, {
   encoding: "utf8"
 })
   .pipe(
@@ -31,7 +111,7 @@ fs.createReadStream("csv/bdav1.csv", {
     })
   )
   .on("end", () => {
-    const ws = fs.createWriteStream("html/bdav1.html", {
+    const ws = fs.createWriteStream(output, {
       encoding: "utf8"
     });
     ws.write(`<html><head>`);
@@ -40,14 +120,13 @@ fs.createReadStream("csv/bdav1.csv", {
     );
     ws.write(`<style type="text/css">${style}</style>`);
     ws.write(`</head>`);
-    ws.write(`<body><div class="wrapper"><div class="column">`);
-    let column = 0;
-    const rows = Math.ceil(results.length / columns);
+    ws.write(`<body><div class="wrapper"><div class="header">`);
+    ws.write(
+      `<h1>${results.length +
+        1}タイトル <span class="dim">(作成日: ${date})</span></h1>`
+    );
+    ws.write(`</div><div class="content">`);
     results.forEach((r, i) => {
-      if (column !== Math.floor(i / rows)) {
-        column = Math.floor(i / rows);
-        ws.write(`</div><div class="column">`);
-      }
       for (let key in r) {
         r[key] = (r[key] as string)
           .replace(/[ａ-ｚＡ-Ｚ０-９]/g, (s: string) => {
@@ -55,8 +134,14 @@ fs.createReadStream("csv/bdav1.csv", {
           })
           .replace(/　/g, " ");
       }
-      ws.write(`<h1>${r.t}</h1><p><small>${r.e}</small></p><p>${r.d}</p>`);
+      ws.write(
+        `<div class="title"><h1><span class="index">${i +
+          1}</span><span class="content">${r.t}</span></h1><p><small>${
+          r.e
+        }</small></p><p>${r.d}</p></div>`
+      );
     });
-    ws.write(`</div></div></body></html>`);
+    ws.write(`</div></div>`);
+    ws.write(`</div></body></html>`);
     ws.close();
   });
